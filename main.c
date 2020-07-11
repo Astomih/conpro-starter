ea110f9271a534d712beb07ec499bb265c380fcf
astomih <asamin1090@gmail.com>
2020-07-11 22:37:28 +0900

    add some function

--- main.c
+++ main.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
-#define REP(i,N) for(i=0;i<N;i++)
-#define REPD(i,N) for(i=N;i>0;i--)
+#define REP(i,N) for(i=0;i<N;i++) //N times repeat. i = increase
+#define REPD(i,N) for(i=N;i>0;i--)//i = decrease 
+
+//A:Array S:Size C:Count var
 #define ARRAY_COUNT(A,S,C) {C=0;int i;REP(i,S){if(*(i+A))C++;}}
 void ctoi(char,int*);	//char to int
 void itoc(int,char*);	//int to char
+char lower(char);		//ex:'A' to 'a'
+char upper(char);		//ex:'a' to 'A'
 
 int main()
 {
-	char c[10] = {"a  "};
-	int count;
-	ARRAY_COUNT(c,10,count);
-	printf("%d",count);
+	char str[32]={};
+	int i,count;
+	scanf("%32s",str);
+	ARRAY_COUNT(str,32,count)
+	REP(i,32){
+		printf("%c",upper(str[i]));
+	}	
+	printf("\n");
 	return 0;
 }
 
@@ -26,3 +34,14 @@ void itoc(int in, char* out)
 	*out = in+48;
 }
 
+char lower(char c)
+{
+	if('A' <= c && c <= 'Z')return c+0x20;
+	return c;
+}
+
+char upper(char c)
+{
+	if('a' <= c && c <= 'z')return c-0x20;
+	return c;
+}
